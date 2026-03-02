653f6609d0a1f14f9b00a5a4264ebeb95f5500bd
lilianne-rdguez <misspadron96@gmail.com>
2026-03-02 12:14:53 +0100

    chore: update macOS folder metadata in .DS_Store.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    image: {
+        domains: ['ximoe124.sg-host.com'],
+    },
+});
